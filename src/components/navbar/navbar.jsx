c9449be37b4691567fc2c17030b6656318251dc2
Cryo79 <johannes@johannes-hreinsson.com>
2022-02-20 11:42:36 +0000

    Working on hamburger menu

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { images } from '../../constants';
-import './navbar.scss';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
-const navbar = () => {
+import { images } from '../../constants';
+import './Navbar.scss';
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -16,8 +21,24 @@ const navbar = () => {
           </li>
           ))}
       </ul>
+
+      <div className='app__navbar-menu'>
+          <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+          {toggle && (
+            <motion.div
+              whileInView={{x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            
+            >
+
+            </motion.div>
+
+          )}
+
+      </div>
     </nav>
   )
 }
 
-export default navbar
+export default Navbar
